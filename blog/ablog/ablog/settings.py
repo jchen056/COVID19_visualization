486e0b329593e90b7fded93974a88dca143831ff
Jerry Chen <jchen056@citymail.cuny.edu>
2023-02-28 22:26:48 -0500

    security resolved

--- blog/ablog/ablog/settings.py
+++ blog/ablog/ablog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-04j0z8px4fa1jazbqdbzppip1%w+80%px@8@^5wmg08rhlb14z"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
